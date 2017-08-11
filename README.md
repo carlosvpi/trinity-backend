429130e25512eb1919f9e5b991efcfce0a1788cd
CarlosVazPI <carlos.vazpi@gmail.com>
2017-08-11 20:49:56 +0100

    park server working

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trinity back end
+
+Root directory: `trinity-backend/my-app`
+
+## Compile
+
+`$ mvn install`
+
+## Run
+
+`$ java -jar target/my-app-1.0-SNAPSHOT.jar`
---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,33 @@
+package com.mycompany.app;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		SparkSession spark = SparkSession
+			.builder()
+			.appName("Java Spark SQL basic example")
+			.master("local")
+			.getOrCreate();
+
+		Dataset<Row> df = spark.read().parquet("../../files/part-00000-584fcd61-b0d2-4429-ac49-078fc31e950a.snappy.parquet");
+		df.printSchema();
+		df.createOrReplaceTempView("table");
+		Dataset<Row> sqlDF;
+		String input = "";
+		Scanner sc = new Scanner(System.in);
+		do {
+			input = sc.nextLine();
+			System.out.println("Type 'quit' to quit");
+			if (!input.equals("quit")) {
+				sqlDF = spark.sql(input); //SELECT COUNT(DISTINCT userId) FROM table
+				sqlDF.show();
+			}
+		} while (!input.equals("quit"));
+
+    }
+}
