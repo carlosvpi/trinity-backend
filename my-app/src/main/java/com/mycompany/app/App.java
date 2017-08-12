46f42e4cc55abdb309519afb36ff1a207e14dc7a
CarlosVazPI <carlos.vazpi@gmail.com>
2017-08-12 15:26:23 +0100

    SQL query well formed

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,5 @@
 package com.mycompany.app;
+import static spark.Spark.*;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -17,17 +18,39 @@ public class App
 		Dataset<Row> df = spark.read().parquet("../../../files/part-00000-584fcd61-b0d2-4429-ac49-078fc31e950a.snappy.parquet");
 		df.printSchema();
 		df.createOrReplaceTempView("table");
-		Dataset<Row> sqlDF;
-		String input = "";
-		Scanner sc = new Scanner(System.in);
-		do {
-			input = sc.nextLine();
-			System.out.println("Type 'quit' to quit");
-			if (!input.equals("quit")) {
-				sqlDF = spark.sql(input); //SELECT COUNT(DISTINCT userId) FROM table
-				sqlDF.show();
-			}
-		} while (!input.equals("quit"));
+        get("/get", (req, res) -> {
+        	String uniq = req.queryParams("uniq");
+        	String period = req.queryParams("period");
+        	String group = req.queryParams("group");
+        	String query = "SELECT COUNT(";
+    		String groupBy = "";
+    		String periodGroupBy = "";
+        	if (uniq.equals("uniq")) {
+        		query += "DISTINCT ";
+        	}
+        	query += "userId) AS value";
+        	if (!group.equals("nothing")) {
+        		query += ", " + group + " AS group";
+        		groupBy = " GROUP BY " + group;
+        	}
+        	if (!period.equals("nothing")) {
+        		periodGroupBy = period.equals("day")
+        			? "SUBSTR(timestamp, 1, 10)"
+        			: period.equals("hour")
+        			? "SUBSTR(timestamp, 12, 19)"
+        			: "timestamp";
+        		query += ", " + periodGroupBy + " AS time";
+        		groupBy = groupBy.equals("")
+        			? " GROUP BY "
+        			: groupBy + ", ";
+        		groupBy += periodGroupBy;
+        	}
+        	query += " FROM table" + groupBy;
+			Dataset<Row> sqlDF = spark.sql(query);
+			sqlDF.show();
+			return "done";
+        	// return sqlDF.toJSON();
+        });
 
     }
 }
